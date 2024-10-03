801db22fdcce731b017e622f3fa8004cbbb12b64
박성현 <qkr970504@gmail.com>
2024-10-03 19:19:15 +0900

    [section04] refactor : OCP
    - 지뢰 찾기 난이도 선택 추가

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -1,9 +1,14 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.gamelevel.VeryBeginner;
+
 public class GameApplication {
 
     public static void main(String[] args) {
-        Minesweeper minesweeper = new Minesweeper();
+        GameLevel gameLevel = new VeryBeginner();
+
+        Minesweeper minesweeper = new Minesweeper(gameLevel);
         minesweeper.run();
     }
 }
